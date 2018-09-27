4f0716a2f70f49935487c55d97346340ba52ae5e
totechite <yutan.h9@gmail.com>
2018-09-28 03:26:48 +0900

    fixed correct

--- examples/sample.rs
+++ examples/sample.rs
@@ -7,8 +7,13 @@ fn main() -> Result<(), String>{
 	let client = Client::set_token(
 		env::var("annict_access_token").unwrap()
 	);
-    let works = annis::works().filter_title("lain").build();
+    let works = annis::works().params(vec![("filter_title", "lain"),("fields","title")]).get();
 	let result = client.call(works)?;
 	println!("{:?}", result["works"]);
+
+	let records = annis::me_records().params(vec![("episode_id", "5013"), ("rating", "5")]).create();
+	let json = client.call(records).unwrap();
+	println!("{:?}", json);
+
 	Ok(())  
 }
