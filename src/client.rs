f3d9ad4cd4d4a335f190238f804a506bb40ee78e
totechite <yutan.h9@gmail.com>
2019-01-19 19:26:30 +0900

    make better

--- src/client.rs
+++ src/client.rs
@@ -1,5 +1,4 @@
-use crate::Service;
-use crate::Value;
+use crate::{Service, Value, Error};
 use serde::Serialize;
 use std::cmp::PartialEq;
 
@@ -47,67 +46,3 @@ impl Client {
     }
 }
 
-#[derive(Fail, Debug)]
-pub enum ErrorKind {
-    #[fail(display = "error: Invalid value at token or request parameters")]
-    InvalidValue,
-}
-
-/* ----------- failure boilerplate ----------- */
-
-use failure::{Backtrace, Context, Fail};
-use std::fmt;
-use std::fmt::Display;
-
-#[derive(Debug)]
-pub struct Error {
-    inner: Context<ErrorKind>,
-}
-
-impl Fail for Error {
-    fn cause(&self) -> Option<&Fail> {
-        self.inner.cause()
-    }
-
-    fn backtrace(&self) -> Option<&Backtrace> {
-        self.inner.backtrace()
-    }
-}
-
-impl Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        Display::fmt(&self.inner, f)
-    }
-}
-
-impl Error {
-    pub fn new(inner: Context<ErrorKind>) -> Error {
-        Error { inner }
-    }
-
-    pub fn kind(&self) -> &ErrorKind {
-        self.inner.get_context()
-    }
-}
-
-impl From<ErrorKind> for Error {
-    fn from(kind: ErrorKind) -> Error {
-        Error {
-            inner: Context::new(kind),
-        }
-    }
-}
-
-impl From<Context<ErrorKind>> for Error {
-    fn from(inner: Context<ErrorKind>) -> Error {
-        Error { inner }
-    }
-}
-
-impl From<reqwest::Error> for Error {
-    fn from(error: reqwest::Error) -> Error {
-        Error {
-            inner: error.context(ErrorKind::InvalidValue),
-        }
-    }
-}
--- src/lib.rs
+++ src/lib.rs
@@ -1225,3 +1225,70 @@ impl fmt::Display for MePrograms {
         write!(f, "{:?}", self)
     }
 }
+
+// -------------------------------------------------
+
+#[derive(Fail, Debug)]
+pub enum ErrorKind {
+    #[fail(display = "error: Invalid value at token or request parameters")]
+    InvalidValue,
+}
+
+/* ----------- failure boilerplate ----------- */
+
+use failure::{Backtrace, Context, Fail};
+use std::fmt::Display;
+
+#[derive(Debug)]
+pub struct Error {
+    inner: Context<ErrorKind>,
+}
+
+impl Fail for Error {
+    fn cause(&self) -> Option<&Fail> {
+        self.inner.cause()
+    }
+
+    fn backtrace(&self) -> Option<&Backtrace> {
+        self.inner.backtrace()
+    }
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        Display::fmt(&self.inner, f)
+    }
+}
+
+impl Error {
+    pub fn new(inner: Context<ErrorKind>) -> Error {
+        Error { inner }
+    }
+
+    pub fn kind(&self) -> &ErrorKind {
+        self.inner.get_context()
+    }
+}
+
+impl From<ErrorKind> for Error {
+    fn from(kind: ErrorKind) -> Error {
+        Error {
+            inner: Context::new(kind),
+        }
+    }
+}
+
+impl From<Context<ErrorKind>> for Error {
+    fn from(inner: Context<ErrorKind>) -> Error {
+        Error { inner }
+    }
+}
+
+impl From<reqwest::Error> for Error {
+    fn from(error: reqwest::Error) -> Error {
+        Error {
+            inner: error.context(ErrorKind::InvalidValue),
+        }
+    }
+}
+
