29a2553b8bb3242e2812b777ca1af3c5bba730d0
totechite <yutan.h9@gmail.com>
2019-01-23 02:21:19 +0900

    change to request the endpoint

--- examples/sample.rs
+++ examples/sample.rs
@@ -1,14 +1,14 @@
 extern crate annis;
-use annis::{Client, Works::*, Error, Value};
+use annis::{Client, Characters::*, Error, Value};
 use std::env;
 
 fn main() -> Result<(), Error> {
     let client = Client::set_token(env::var("annict_access_token").unwrap());
 
-    let params = vec![(filter_title, "lain"), (fields, "title")];
-    let works = annis::works().params(params);
-    let json = client.call(works)?.json::<Value>()?;
-    println!("{:?}", json);
+    let params = vec![(filter_ids, "26233"), (fields, "name")];
+    let characters = annis::characters().params(params);
+    let json = client.call(characters)?.json::<Value>()?;
+    println!("{:?}", json["characters"][0]["name"]);
 
     Ok(())
 }
